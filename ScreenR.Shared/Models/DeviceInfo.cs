c9d167d9aeb4de428530f80b1c34ec2a357bbf46
Jared Goodwin <translucency@outlook.com>
2022-06-10 16:41:34 -0700

    Rename title.

--- ScreenR.Shared/Models/DeviceInfo.cs
+++ ScreenR.Shared/Models/DeviceInfo.cs
@@ -4,33 +4,45 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ScreenR.Shared.Models
 {
+    [DataContract]
     public class DeviceInfo
     {
         public static DeviceInfo Empty { get; } = new DeviceInfo();
 
+        [DataMember]
         public Architecture Architecture { get; init; }
 
+        [DataMember]
         public string ComputerName { get; init; } = string.Empty;
 
+        [DataMember]
         public Guid DeviceId { get; init; }
 
+        [DataMember]
         public bool Is64Bit { get; init; }
 
+        [DataMember]
         public bool IsOnline { get; init; } = true;
 
+        [DataMember]
         public string OperatingSystem { get; init; } = string.Empty;
 
+        [DataMember]
         public Platform Platform { get; init; }
 
+        [DataMember]
         public int ProcessorCount { get; init; }
 
+        [DataMember]
         public Guid SessionId { get; init; }
 
+        [DataMember]
         public ConnectionType Type { get; init; }
 
 
