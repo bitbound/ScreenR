7728c8c21a761c7fa5876db80ad8050beb82fad0
Jared Goodwin <translucency@outlook.com>
2022-06-18 11:55:43 -0700

    Add full-screen loader.  Put login components in nav menu.

---
+++ ScreenR.Shared/DisposalToken.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenR.Shared
+{
+    public sealed class DisposalToken : IDisposable
+    {
+        private readonly Action _disposeCallback;
+
+        public DisposalToken(Action disposeCallback)
+        {
+            _disposeCallback = disposeCallback;
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                _disposeCallback();
+            }
+            catch { }
+        }
+    }
+}
